a382b4d5bac520f6c21bb8781c8d8eeab6772fe3
beris147 <edwir147@gmail.com>
2022-02-12 14:44:18 -0800

    add .gitignore

--- hello_world/target/rls/debug/deps/hello_world-d2cd15b9fcc2a615.d
+++
@@ -1,5 +0,0 @@
-/home/beristain/learning_rust/hello_world/target/rls/debug/deps/hello_world-d2cd15b9fcc2a615.rmeta: src/main.rs
-
-/home/beristain/learning_rust/hello_world/target/rls/debug/deps/hello_world-d2cd15b9fcc2a615.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/rls/debug/deps/hello_world-f75324aec22957ab.d
+++
@@ -1,5 +0,0 @@
-/home/beristain/learning_rust/hello_world/target/rls/debug/deps/hello_world-f75324aec22957ab.rmeta: src/main.rs
-
-/home/beristain/learning_rust/hello_world/target/rls/debug/deps/hello_world-f75324aec22957ab.d: src/main.rs
-
-src/main.rs:
