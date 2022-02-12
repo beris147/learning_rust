cfae42ba4bb2b056a11c7c1b4cbaad9dac5b7b7e
beris147 <edwir147@gmail.com>
2022-02-12 14:41:14 -0800

    cargo hello_world

---
+++ hello_world/target/rls/debug/deps/hello_world-d2cd15b9fcc2a615.d
@@ -0,0 +1,5 @@
+/home/beristain/learning_rust/hello_world/target/rls/debug/deps/hello_world-d2cd15b9fcc2a615.rmeta: src/main.rs
+
+/home/beristain/learning_rust/hello_world/target/rls/debug/deps/hello_world-d2cd15b9fcc2a615.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/rls/debug/deps/hello_world-f75324aec22957ab.d
@@ -0,0 +1,5 @@
+/home/beristain/learning_rust/hello_world/target/rls/debug/deps/hello_world-f75324aec22957ab.rmeta: src/main.rs
+
+/home/beristain/learning_rust/hello_world/target/rls/debug/deps/hello_world-f75324aec22957ab.d: src/main.rs
+
+src/main.rs:
